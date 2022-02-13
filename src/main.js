411a58be8334bf08893860dc8e4cece73f4587a5
manhgg <mauricio.nahun.gg@gmail.com>
2022-02-13 00:47:15 -0600

    Add: Project Creation Files/First Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Vue from "vue";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import { Bootstrap } from "bootstrap";
 
-createApp(App).mount('#app')
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+Vue.use(Bootstrap);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
+createApp(App).use(router).mount("#app");
